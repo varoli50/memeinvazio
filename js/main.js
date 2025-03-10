cfeacfa28b9e8a707343e8074998c47d54f04ffa
tanulo <tanulo@barossgabor.local>
2025-03-10 10:05:18 +0100

    index.html done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const btnLogin=document.getElementsByClassName('login')[0]
+const btnReg = document.getElementsByClassName('reg')[0]
+
+btnLogin.addEventListener('click', () => {
+    window.location.href= '../login.html'
+})
+
+btnReg.addEventListener('click', () => {
+    window.location.href='../reg.html'
+})
